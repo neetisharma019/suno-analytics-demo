c4cd48cb4d646491405125fcc4ef249ab4109485
neetisharma019 <105536773+neetisharma019@users.noreply.github.com>
2026-01-11 19:40:15 -0500

    Initial commit: Suno analytics engineering project

--- suno_analytics/target/run/suno_analytics/models/core/dim_user.sql
+++ suno_analytics/target/run/suno_analytics/models/core/dim_user.sql
@@ -13,32 +13,23 @@
 
 subscriptions as (
     select * from "suno_analytics"."main"."stg_subscriptions"
-),
-
-first_song_events as (
-    select
-        user_id,
-        min(event_timestamp) as first_song_timestamp
-    from "suno_analytics"."main"."stg_events"
-    where event_type = 'song_generated'
-    group by user_id
 )
 
 select
     u.user_id,
     u.signup_timestamp,
     u.signup_date,
-    u.marketing_channel,
     u.region,
+    u.acquisition_channel,
     u.is_creator,
-    fse.first_song_timestamp,
-    s.plan_tier,
-    s.is_active as subscription_is_active,
-    s.current_period_start,
-    s.current_period_end
+    u.platform,
+    s.plan,
+    s.subscription_start,
+    s.subscription_end,
+    s.is_trial,
+    s.converted_to_paid,
+    s.conversion_timestamp
 from users u
-left join first_song_events fse
-    on u.user_id = fse.user_id
 left join subscriptions s
     on u.user_id = s.user_id
     );
